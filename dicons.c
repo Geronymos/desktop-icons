3f95106227800045fc96d1ac698997f3ee9462d3
Orangerot <purple@orangerot.dev>
2023-04-05 15:53:13 +0200

    read name and icon from files on desktop

--- dicons.c
+++ dicons.c
@@ -1,13 +1,32 @@
 #include <gtk/gtk.h>
+#include <gio/gio.h>
 
 static void activate (GtkApplication* app, gpointer user_data)
 {
-    GtkWidget *window;
+    GtkWidget *window, *iconview;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW (window), "Window");
     gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
-    gtk_widget_show(window);
+
+    // iconview = gtk_grid_view_new(gtk_single_selection_new(0), );
+    // gtk_window_set_child(GTK_WINDOW (window), iconview);
+
+    GFile *path = g_file_parse_name("~/desktop");
+    GFileEnumerator *children = g_file_enumerate_children(path, "standard::*,ownser::user", 0, 0, 0);
+    
+    GtkIconTheme *theme = gtk_icon_theme_get_for_display(gtk_widget_get_display(window));
+
+    GFileInfo *info;
+    while ( (info = g_file_enumerator_next_file(children, 0, 0)) ) {
+        GIcon *icon = g_file_info_get_icon(info);
+        const char *name = g_file_info_get_name(info);
+        GtkIconPaintable *pixbuf = gtk_icon_theme_lookup_by_gicon(theme, icon, 48, 1, 0, 0);
+
+        printf("%s\n", name);
+    }
+
+    gtk_window_present(GTK_WINDOW (window));
 }
 
 int main (int argc, char **argv)
